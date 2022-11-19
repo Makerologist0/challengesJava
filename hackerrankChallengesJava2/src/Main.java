edeaf80b4f6e4e1d30d91f79dca23bcf91e18390
Furkan DOLUNAY <105243168+Makerologist0@users.noreply.github.com>
2022-11-19 22:29:37 +0300

    Add files via upload

---
+++ hackerrankChallengesJava2/src/Main.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+
+                /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+
+                Scanner scanner = new Scanner(System.in);
+                int myInt1 = scanner.nextInt();
+                int myInt2 = scanner.nextInt();
+                int myInt3 = scanner.nextInt();
+                System.out.println(myInt1);
+                System.out.println(myInt2);
+                System.out.println(myInt3);
+
+
+
+
+
+
+
+
+
+
+    }
+}
