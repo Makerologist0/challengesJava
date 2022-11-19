3302679c0b73c55793616dd2cbd4ee3da4139eac
Furkan DOLUNAY <105243168+Makerologist0@users.noreply.github.com>
2022-11-19 22:43:16 +0300

    Add files via upload

---
+++ hackerrankChallengesJava3/src/Main.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        bufferedReader.close();
+        if(N % 2 == 1){
+            System.out.println("Weird");
+        }
+        else
+            System.out.println("Not Weird");
+    }
+}
+
+
+
+
+
+
+
